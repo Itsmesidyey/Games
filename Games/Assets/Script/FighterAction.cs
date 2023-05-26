e9e1a4b9132eda722cfae6df1a11ff86ec7a8498
Itsmesidyey <119683075+Itsmesidyey@users.noreply.github.com>
2023-05-27 03:25:24 +0800

    Game-Part3

---
+++ Games/Assets/Script/FighterAction.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterAction : MonoBehaviour
+{
+    private GameObject enemy;
+    private GameObject hero;
+
+    [SerializeField]
+    private GameObject CounterPunchPrefab;
+
+    [SerializeField]
+    private GameObject BlockPrefab;
+
+    [SerializeField]
+    private GameObject BodyshotPrefab;
+
+    [SerializeField]
+    private GameObject ClinchPrefab;
+
+    [SerializeField]
+    private GameObject CrossPrefab;
+
+    [SerializeField]
+    private GameObject FootworkPrefab;
+
+    [SerializeField]
+    private GameObject HookPrefab;
+
+    [SerializeField]
+    private GameObject JabPrefab;
+
+    [SerializeField]
+    private GameObject SlipPrefab;
+
+    [SerializeField]
+    private GameObject UppercutPrefab;
+
+    [SerializeField]
+    private GameObject faceIcon;
+
+    private GameObject currentAttack;
+    private GameObject CounterPunchAttack;
+    private GameObject BlockAttack;
+    private GameObject BodyshotAttack;
+    private GameObject ClinchAttack;
+    private GameObject CrossAttack;
+    private GameObject FootworkAttack;
+    private GameObject HookAttack;
+    private GameObject JabAttack;
+    private GameObject SlipAttack;
+    private GameObject UppercutAttack;
+
+    public void SelectAttack(string btn)
+    {
+        GameObject victim = tag == "Hero" ? enemy : hero;
+        if (tag == "Hero") 
+        {
+            victim = enemy;
+        }
+        if (btn.CompareTo("CounterPunch") == 0)
+        {
+            CounterPunchAttack.GetComponent<AttackScript>().Attack(victim);
+        }
+        else if (btn.CompareTo("Block") == 0)
+        {
+            BlockAttack.GetComponent<AttackScript>().Attack(victim);
+        }
+        else if (btn.CompareTo("BodyShot") == 0)
+        {
+            BodyshotAttack.GetComponent<AttackScript>().Attack(victim);
+        }
+        else if (btn.CompareTo("Clinch") == 0)
+        {
+            ClinchAttack.GetComponent<AttackScript>().Attack(victim);
+        }
+        else if (btn.CompareTo("Cross") == 0)
+        {
+            CrossAttack.GetComponent<AttackScript>().Attack(victim);
+        }
+        else if (btn.CompareTo("FootWork") == 0)
+        {
+            FootworkAttack.GetComponent<AttackScript>().Attack(victim);
+        }
+        else if (btn.CompareTo("Hook") == 0)
+        {
+            HookAttack.GetComponent<AttackScript>().Attack(victim);
+        }
+        else if (btn.CompareTo("Jab") == 0)
+        {
+            JabAttack.GetComponent<AttackScript>().Attack(victim);
+        }
+        else if (btn.CompareTo("Slip") == 0)
+        {
+           SlipAttack.GetComponent<AttackScript>().Attack(victim);
+        }
+        else
+        {
+            UppercutAttack.GetComponent<AttackScript>().Attack(victim);
+        }
+    }
+}
--- Games/Assets/Script/MakeButton.cs
+++ Games/Assets/Script/MakeButton.cs
@@ -7,16 +7,57 @@ public class MakeButton : MonoBehaviour
 {
     [SerializeField]
     private bool physical;
+    private GameObject hero;
     // Start is called before the first frame update
     void Start()
     {
         string temp = gameObject.name;
-        gameObject.GetComponent<Button>().onClick.AddListener(() +> AttachCallback(temp));
+        gameObject.GetComponent<Button>().onClick.AddListener(() => AttachCallback(temp));
+        hero = GameObject.FindGameObjectWithTag("Hero");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AttachCallback(string btn)
     {
-        
+        if(btn.CompareTo("CounterPunchbtn") == 0)
+        {
+            hero.GetComponent<FighterAction>().SelectAttack("CounterPunch");
+        } 
+        else if (btn.CompareTo("Blockbtn") == 0)
+        {
+            hero.GetComponent<FighterAction>().SelectAttack("Block");
+        }
+        else if (btn.CompareTo("Bodyshotbtn") == 0)
+        {
+            hero.GetComponent<FighterAction>().SelectAttack("BodyShot");
+        }
+        else if (btn.CompareTo("Clinchbtn") == 0)
+        {
+            hero.GetComponent<FighterAction>().SelectAttack("Clinch");
+        }
+        else if (btn.CompareTo("Crossbtn") == 0)
+        {
+            hero.GetComponent<FighterAction>().SelectAttack("Cross");
+        }
+        else if (btn.CompareTo("Footworkbtn") == 0)
+        {
+            hero.GetComponent<FighterAction>().SelectAttack("FootWork");
+        }
+        else if (btn.CompareTo("Hookbtn") == 0)
+        {
+            hero.GetComponent<FighterAction>().SelectAttack("Hook");
+        }
+        else if (btn.CompareTo("Jabbtn") == 0)
+        {
+            hero.GetComponent<FighterAction>().SelectAttack("Jab");
+        }
+        else if (btn.CompareTo("Slipbtn") == 0)
+        {
+            hero.GetComponent<FighterAction>().SelectAttack("Slip");
+        }
+        else
+        {
+            hero.GetComponent<FighterAction>().SelectAttack("UpperCut");
+        }
     }
 }
+
